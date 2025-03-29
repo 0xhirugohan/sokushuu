7faeff40de1bcf83658b3feea0abacfd22566657
han <han@fbrns.co>
2025-03-30 00:30:36 +0700

    fix linter

--- packages/sokushuu-app/src/main.tsx
+++ packages/sokushuu-app/src/main.tsx
@@ -3,7 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+function getOrSetRootElement(): HTMLElement {
+  if (!document.getElementById('root')) {
+    const rootDiv = document.createElement('div');
+    rootDiv.setAttribute('id', 'root');
+    document.body.appendChild(rootDiv);
+  }
+
+  return document.getElementById('root') || document.createElement('div');
+}
+
+const root = getOrSetRootElement();
+
+createRoot(root).render(
   <StrictMode>
     <App />
   </StrictMode>,
