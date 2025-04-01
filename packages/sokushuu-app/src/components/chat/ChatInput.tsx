fd7c69e6c033109f2a35ddcc1715e10923e4cfb3
han <han@fbrns.co>
2025-04-01 18:23:41 +0700

    add loading

--- packages/sokushuu-app/src/components/chat/ChatInput.tsx
+++ packages/sokushuu-app/src/components/chat/ChatInput.tsx
@@ -2,18 +2,23 @@ import { useState } from "react";
 
 import { Textarea } from "@/components/ui/textarea";
 import SendChatIcon from "@/assets/send-chat.svg";
+import LoadingIcon from "@/assets/loading.svg";
 import { generateContent } from "@/lib/gemini";
 
 import type { IChatHistory } from "@/components/layouts/ChatMenu";
 
 const ChatInput = ({ className, onMessageSent }: { className?: string, onMessageSent: (messages: IChatHistory[]) => void }) => {
     const [message, setMessage] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
     const onMessageTyping = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setMessage(e.target.value);
     }
 
     const sendMessage = async () => {
+        if (isLoading) return;
+
+        setIsLoading(true);
         const newMessages: IChatHistory[] = [{message, isUser: true, timestamp: new Date().toISOString()}];
         const response = await generateContent(message);
         for (const candidate of response.candidates) {
@@ -23,6 +28,7 @@ const ChatInput = ({ className, onMessageSent }: { className?: string, onMessage
         }
         onMessageSent(newMessages);
         setMessage("");
+        setIsLoading(false);
     }
 
     const handleKeyUp = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
@@ -33,6 +39,7 @@ const ChatInput = ({ className, onMessageSent }: { className?: string, onMessage
 
     return <div className={`${className} flex`}>
         <Textarea
+            disabled={isLoading}
             onKeyUp={handleKeyUp}
             onChange={onMessageTyping}
             value={message}
@@ -40,7 +47,7 @@ const ChatInput = ({ className, onMessageSent }: { className?: string, onMessage
             placeholder="How can I help you today?"
         />
         <button onClick={sendMessage} type="button" className="p-2 flex items-end justify-end cursor-pointer hover:opacity-60">
-            <img src={SendChatIcon} alt="send chat" />
+            {isLoading ? <img className="animate-spin" src={LoadingIcon} alt="loading" /> : <img src={SendChatIcon} alt="send chat" />}
         </button>
     </div>
 }
