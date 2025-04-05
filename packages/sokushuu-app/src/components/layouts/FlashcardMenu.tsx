9d89da323edb1dd67142d044ec7e472ae2562f7c
han <han@fbrns.co>
2025-04-05 21:28:10 +0700

    add overlay create collection form

--- packages/sokushuu-app/src/components/layouts/FlashcardMenu.tsx
+++ packages/sokushuu-app/src/components/layouts/FlashcardMenu.tsx
@@ -1,11 +1,26 @@
+import React, { useState } from "react";
+
 import { Title } from "@/components/flashcards/Title";
+
+import { CreateCollectionPopup } from "@/components/form/CreateCollectionPopup";
 import { HomeContent } from "@/components/layouts/HomeContent";
 import { AllContentCard } from "@/components/layouts/AllContentCard";
 
 const FlashcardMenu = ({ styleName, searchQueryProps }: { styleName?: string, searchQueryProps?: string }) => {
-    return <div className={`${styleName} p-4 w-full h-full flex flex-col gap-y-8 overflow-x-hidden`}>
+    const [isCreateCollectionPopupOpen, setIsCreateCollectionPopupOpen] = useState(false);
+
+    const showCreateCollectionPopup = () => {
+        setIsCreateCollectionPopupOpen(true);
+    }
+
+    const closeCreateCollectionPopup = () => {
+        setIsCreateCollectionPopupOpen(false);
+    }
+
+    return <div className={`${styleName} p-4 w-full h-full flex flex-col gap-y-8 overflow-x-hidden relative`}>
         <Title searchQueryProps={searchQueryProps} />
-        { searchQueryProps ? <AllContentCard /> : <HomeContent /> }
+        { searchQueryProps ? <AllContentCard /> : <HomeContent showCreateCollectionPopup={showCreateCollectionPopup} /> }
+        { isCreateCollectionPopupOpen && <CreateCollectionPopup onClose={closeCreateCollectionPopup} /> }
     </div>
 }
 
--- packages/sokushuu-app/src/components/layouts/HomeContent.tsx
+++ packages/sokushuu-app/src/components/layouts/HomeContent.tsx
@@ -4,7 +4,11 @@ import { CollectionCard } from "@/components/card/CollectionCard";
 
 import PlusIcon from "@/assets/plus.svg";
 
-const HomeContent: React.FC = () => {
+interface HomeContentProps {
+    showCreateCollectionPopup: () => void;
+}
+
+const HomeContent: React.FC<HomeContentProps> = ({ showCreateCollectionPopup }: HomeContentProps) => {
     const [hasCreatedCollection, setHasCreatedCollection] = useState(false);
     const [hasViewedCollection, setHasViewedCollection] = useState(false);
 
@@ -13,7 +17,7 @@ const HomeContent: React.FC = () => {
         { hasViewedCollection && <CollectionCard collectionName="Recently Viewed" /> }
         { hasCreatedCollection ? 
             <CollectionCard collectionName="Recently Created" />
-            : <button type="button" className="h-20 md:h-40 border-2 border-zinc-500 rounded-lg flex gap-x-6 items-center justify-center cursor-pointer">
+            : <button onClick={showCreateCollectionPopup} type="button" className="h-20 md:h-40 border-2 border-zinc-500 rounded-lg flex gap-x-6 items-center justify-center cursor-pointer">
                 <span className="text-2xl">Create New</span>
                 <img className="p-1 w-12 h-12 md:w-16 md:h-16 border-2 border-zinc-500 rounded-full" src={PlusIcon} alt="plus" />
             </button>
