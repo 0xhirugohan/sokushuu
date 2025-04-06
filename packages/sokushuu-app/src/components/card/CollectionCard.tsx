4802b3e062a380c0e28f8e92fa0e4eeea339b09f
han <han@fbrns.co>
2025-04-06 20:10:06 +0700

    navigate see more card to collection detail

--- packages/sokushuu-app/src/components/card/CollectionCard.tsx
+++ packages/sokushuu-app/src/components/card/CollectionCard.tsx
@@ -1,19 +1,29 @@
 import React from "react";
+import { NavLink } from 'react-router';
 
-interface CollectionCardProps {
-    collectionName: string;
+type Collection = {
+    slug: string;
+    name: string;
 }
 
-const CollectionCard: React.FC<CollectionCardProps> = ({ collectionName }) => {
+interface CollectionCardProps {
+    collection: Collection;
+}
+
+const CollectionCard: React.FC<CollectionCardProps> = ({ collection }) => {
     return <div className="w-full flex flex-col gap-y-2">
-        <div className="font-semibold">{collectionName}</div>
+        <div className="font-semibold">{collection.name}</div>
         <div className="w-[82vw] overflow-x-scroll">
             <div className="w-fit flex gap-x-2 md:gap-x-4 h-20 md:h-40">
                 <div className="h-full w-[32vw] md:w-[20vw] rounded-lg border-2 border-zinc-500">square 1</div>
                 <div className="h-full w-[32vw] md:w-[20vw] rounded-lg border-2 border-zinc-500">square 2</div>
-                <div className="h-full w-[32vw] md:w-[20vw] rounded-lg border-2 border-zinc-500">See more</div>
-                <div className="h-full w-[32vw] md:w-[20vw] rounded-lg border-2 border-zinc-500">See more</div>
-                <div className="h-full w-[32vw] md:w-[20vw] rounded-lg border-2 border-zinc-500 flex justify-center items-center">See more</div>
+                <div className="h-full w-[32vw] md:w-[20vw] rounded-lg border-2 border-zinc-500">square 3</div>
+                <NavLink
+                    to={`/collection/${collection.slug}`}
+                    className="h-full w-[32vw] md:w-[20vw] rounded-lg border-2 border-zinc-500 flex justify-center items-center"
+                >
+                    See more
+                </NavLink>
             </div>
         </div>
     </div>
--- packages/sokushuu-app/src/components/layouts/HomeContent.tsx
+++ packages/sokushuu-app/src/components/layouts/HomeContent.tsx
@@ -12,11 +12,26 @@ const HomeContent: React.FC<HomeContentProps> = ({ showCreateCollectionPopup }: 
     const [hasCreatedCollection, setHasCreatedCollection] = useState(false);
     const [hasViewedCollection, setHasViewedCollection] = useState(false);
 
+    const exploreCollection = {
+        slug: "explore",
+        name: "Explore"
+    }
+
+    const recentlyViewedCollection = {
+        slug: "recently-viewed",
+        name: "Recently Viewed"
+    }
+
+    const recentlyCreatedCollection = {
+        slug: "recently-created",
+        name: "Recently Created"
+    }
+
     return <>
-        <CollectionCard collectionName="Explore" />
-        { hasViewedCollection && <CollectionCard collectionName="Recently Viewed" /> }
+        <CollectionCard collection={exploreCollection} />
+        { hasViewedCollection && <CollectionCard collection={recentlyViewedCollection} /> }
         { hasCreatedCollection ? 
-            <CollectionCard collectionName="Recently Created" />
+            <CollectionCard collection={recentlyCreatedCollection} />
             : <button onClick={showCreateCollectionPopup} type="button" className="h-20 md:h-40 border-2 border-zinc-500 rounded-lg flex gap-x-6 items-center justify-center cursor-pointer">
                 <span className="text-2xl">Create New</span>
                 <img className="p-1 w-12 h-12 md:w-16 md:h-16 border-2 border-zinc-500 rounded-full" src={PlusIcon} alt="plus" />
