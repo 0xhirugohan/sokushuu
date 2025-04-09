e2ed2adb1c468a4b9a7115f1a97d6334b71064d6
hirugohan <han@fbrns.co>
2025-04-09 15:43:48 +0700

    add contract deployment and write function call from frontend (#9)

--- packages/sokushuu-app/src/lib/wallet.ts
+++ packages/sokushuu-app/src/lib/wallet.ts
@@ -1,6 +1,15 @@
-import { createPublicClient, createWalletClient, custom, http, formatEther } from 'viem';
+import {
+    createPublicClient,
+    createWalletClient,
+    custom,
+    http,
+    formatEther,
+    parseEther,
+} from 'viem';
 
 import { educhainTestnet } from './chain';
+import { wagmiAbi, wagmiBytecode } from './abi/CollectionOwnershipNFT';
+import { SOKUSHUU_MINT_SALE_ADDRESS } from './constant';
 
 export const walletClient = createWalletClient({
     chain: educhainTestnet,
@@ -18,3 +27,39 @@ export const getAddressBalance = async (address: `0x${string}`) => {
     const balanceInEther = formatEther(balance);
     return balanceInEther;
 };
+
+export const getAccount = async () => {
+    const accounts = await walletClient.getAddresses();
+    return accounts[0];
+};
+
+export const deployNFTContract = async (collectionTitle: string, collectionSymbol: string, price: number): Promise<`0x${string}`> => {
+    const account = await getAccount();
+    const hash = await walletClient.deployContract({
+        abi: wagmiAbi,
+        account,
+        args: [
+            collectionTitle,
+            collectionSymbol,
+            parseEther(`${price}`, "wei"),
+            SOKUSHUU_MINT_SALE_ADDRESS,
+        ],
+        bytecode: wagmiBytecode,
+    });
+
+    const receipt = await publicClient.getTransactionReceipt({ hash });
+    const contractAddress = receipt.contractAddress;
+    return contractAddress ?? "0x0000000000000000000000000000000000000000";
+}
+
+export const registerDeployedNFTContract = async (contractAddress: `0x${string}`): Promise<`0x${string}`> => {
+    const account = await getAccount();
+    const { request } = await publicClient.simulateContract({
+        account,
+        address: contractAddress,
+        abi: wagmiAbi,
+        functionName: 'register',
+    });
+    const hash = await walletClient.writeContract(request);
+    return hash;
+}
